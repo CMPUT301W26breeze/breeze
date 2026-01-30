c293bcad2858c0c687536773dfcb71fa6c744242
Ahmed Jama <ahmedjama@Ahmeds-MacBook-Air-42.local>
2026-01-29 21:50:43 -0700

    Add Java class that is named Pentagon and extends from Shape class

--- Code/app/src/main/java/com/example/breeze/Pentagon.java
+++ Code/app/src/main/java/com/example/breeze/Pentagon.java
@@ -1,34 +1,12 @@
 package com.example.breeze;
 
 public class Pentagon extends Shape {
-    private int x;
-    private int y;
 
-    public Pentagon(){
+    public Pentagon() {
         super();
     }
-    public Pentagon(int x, int y, int x1) {
+
+    public Pentagon(int x, int y) {
         super(x, y);
-        this.x = x1;
-    }
-
-    @Override
-    public int getX() {
-        return x;
-    }
-
-    @Override
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    @Override
-    public int getY() {
-        return y;
-    }
-
-    @Override
-    public void setY(int y) {
-        this.y = y;
     }
 }
