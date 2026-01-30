0933f2eb424c6bfc18f2c810775fec1ec8ebcd7a
ualbertagit123 <guanghua@ualberta.ca>
2026-01-29 21:58:37 -0700

    Modified Triangle class

--- Code/app/src/main/java/com/example/breeze/Triangle.java
+++ Code/app/src/main/java/com/example/breeze/Triangle.java
@@ -1,5 +1,10 @@
 package com.example.breeze;
 
 public class Triangle extends Shape {
-
+    public Triangle() {
+        super();
+    }
+    public Triangle(int x, int y) {
+        super(x, y);
+    }
 }
